128cc2f03ca19a0194a420e3169110c7a0b429e0
KevinSoro <kevingiovani25@gmail.com>
2020-04-14 05:24:36 -0500

    error al subir

--- roles/Controller/controllerDescarga.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$nombreDeArchivoDescarga = $_GET['nombreDecarga'];
-echo $nombreDeArchivoDescarga;
-
-	header("Content-type: application/PDF");
-	readfile("./documentos/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
-	
-?>
