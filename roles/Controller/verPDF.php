a9eba677cf64e6a1c7b8c6a3a571bb67fed61d88
kevinsolanorodriguez <kevingiovani25@gmail.com>
2020-04-16 10:45:46 -0500

    borrado proyecto

--- roles/Controller/verPDF.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$nombreDeArchivoDescarga = $_POST['nombreDecarga'];
-echo $nombreDeArchivoDescarga;
-
-	header("Content-type: application/PDF");
-	readfile("./documentos/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
-	
-?>
